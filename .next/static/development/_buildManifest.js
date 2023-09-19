b3774f09ad6443aa1a4bcbb8fe06cc14560cff77
유정인 <124610396+jaylenyu@users.noreply.github.com>
2023-09-19 23:37:02 +0900

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/products/page":["static\u002Fchunks\u002Fpages\u002Fproducts\u002Fpage.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fproducts\u002Fpage"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

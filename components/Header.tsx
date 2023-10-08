3493b471d05fcfb0b134ad4c1a7776fa17eb20d4
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-10-08 20:26:53 +0900

    fix header layout & add home

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,41 +9,52 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <div className="fixed w-full flex justify-between gap-5 h-20 bg-white px-5 shadow-xl z-50">
-      <Image
-        className="hover:cursor-pointer"
-        src="/Logo.png"
-        alt="Logo"
-        priority
-        unoptimized
-        width={250}
-        height={70}
-        onClick={() => router.push("/")}
-      />
-      <ul className="flex gap-5 items-center">
-        {session?.user?.name && session?.user?.image && (
-          <>
-            <div className="flex items-center">
-              <img
-                className="w-10 h-10 rounded-full mr-3"
-                src={session.user.image}
-                alt="user"
-              />
+    <div className="w-full relative">
+      <div className="fixed w-full flex justify-between gap-5 h-20 bg-white px-5 shadow-xl z-50">
+        <ul className="flex gap-5 items-center">
+          <HeaderList onClick={() => router.push("/products")}>
+            제품보기
+          </HeaderList>
+          <HeaderList onClick={() => router.push("/wishlist")}>
+            위시리스트
+          </HeaderList>
+          <HeaderList onClick={() => router.push("/cart")}>장바구니</HeaderList>
+          <HeaderList onClick={() => router.push("/order")}>
+            주문내역
+          </HeaderList>
+        </ul>
+        <div className="absolute left-1/2 -translate-x-1/2">
+          <Image
+            className="hover:cursor-pointer"
+            src="/Logo.png"
+            alt="Logo"
+            priority
+            unoptimized
+            width={250}
+            height={80}
+            onClick={() => router.push("/")}
+          />
+        </div>
+        <ul className="flex gap-5 items-center">
+          {session?.user?.name && session?.user?.image && (
+            <>
+              <HeaderList>
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={session.user.image}
+                  alt="user"
+                />
+              </HeaderList>
               <HeaderList>{session?.user?.name}님</HeaderList>
-            </div>
-          </>
-        )}
-        <HeaderList onClick={() => router.push("/wishlist")}>
-          위시리스트
-        </HeaderList>
-        <HeaderList onClick={() => router.push("/cart")}>장바구니</HeaderList>
-        <HeaderList onClick={() => router.push("/order")}>주문내역</HeaderList>
-        {session ? (
-          <HeaderList onClick={() => signOut()}>로그아웃</HeaderList>
-        ) : (
-          <HeaderList onClick={() => signIn()}>로그인</HeaderList>
-        )}
-      </ul>
+            </>
+          )}
+          {session ? (
+            <HeaderList onClick={() => signOut()}>로그아웃</HeaderList>
+          ) : (
+            <HeaderList onClick={() => signIn()}>로그인</HeaderList>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
